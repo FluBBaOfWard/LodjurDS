52a4827ccfdb041134b539fa4206c2884723e6f0
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2025-01-05 13:58:30 +0100

    Some clean up of comments regarding Timer Done bit, updated history and todo lists.

--- source/Gui.c
+++ source/Gui.c
@@ -14,7 +14,7 @@
 #include "ARMMikey/Version.h"
 #include "ARMSuzy/Version.h"
 
-#define EMUVERSION "V0.1.0 2025-01-04"
+#define EMUVERSION "V0.1.0 2025-01-05"
 
 static void gammaChange(void);
 static void machineSet(void);
